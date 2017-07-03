322d02811c7f64a4b2d822aeb7bbb601860bbd0e
Lukasz Filipiak <l.filipiak@bkf.pl>
2017-07-03 13:30:42 +0200

    From very begining

--- response.php
+++ response.php
@@ -6,11 +6,7 @@ require_once('config/query.dbmysql.php');
 $DB=new dbmysqldriver();
 
 
-<<<<<<< HEAD
 $result2=$DB->query('select * from supply_order');
-=======
-$result2=$DB->query($mainquery_demo);
->>>>>>> 3dc9981220f61a5f73871d151fa2f804261f1265
 
 
 //foreach($result2 as $key)
